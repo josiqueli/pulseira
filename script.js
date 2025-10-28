76b2dafe584f9772fbadf418606fe34039d6a97a
josiqueli <105284716+josiqueli@users.noreply.github.com>
2025-10-28 14:38:54 -0300

    k

---
+++ script.js
@@ -0,0 +1,23 @@
+const distancia = document.getElementById("distancia");
+const led = document.getElementById("led");
+const status = document.getElementById("status");
+
+distancia.addEventListener("input", () => {
+  const valor = parseInt(distancia.value);
+  status.textContent = `Distância: ${valor}m`;
+
+  // Mudança de cor conforme a distância
+  if (valor < 30) {
+    led.style.background = "green";
+    led.style.boxShadow = "0 0 20px green";
+    led.classList.remove("vibrando");
+  } else if (valor < 70) {
+    led.style.background = "yellow";
+    led.style.boxShadow = "0 0 20px yellow";
+    led.classList.remove("vibrando");
+  } else {
+    led.style.background = "red";
+    led.style.boxShadow = "0 0 25px red";
+    led.classList.add("vibrando");
+  }
+});
